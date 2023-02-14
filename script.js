6d54d1a8d496b21e27111f308410414eae787705
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-02-13 19:10:43 -0700

    unknow

--- script.js
+++ script.js
@@ -19,7 +19,12 @@ const renderCountry = function (data, className = "") {
   </article>
   `;
   countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText("beforeend", msg);
+  // countriesContainer.style.opacity = 1;
 };
 
 ///////////////////////////////////////
@@ -141,4 +146,33 @@ const renderCountry = function (data, className = "") {
 // };
 // getCountryData("portugal");
 
-//*
+//* handling rejected promises *//
+const getCountryData = function (country) {
+  // country 1
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then((response) => response.json())
+    .then((data) => renderCountry(data, "neighbour"))
+    .catch((err) => {
+      console.error(`${err}💥💥💥`);
+      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener("click", function () {
+  getCountryData("mexico");
+});
+
+// getCountryData("gkjdhfgkj");
